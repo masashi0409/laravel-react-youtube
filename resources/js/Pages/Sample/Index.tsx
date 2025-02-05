b5ce2e56a1438526daa069afe050ad940979e80b
shimano masashi <masashi.shimano0409@gmail.com>
2025-02-05 21:37:53 +0900

    MUIインストール

--- resources/js/Pages/Sample/Index.tsx
+++ resources/js/Pages/Sample/Index.tsx
@@ -1,26 +1,54 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
+import { Button, Container, Typography, Box } from "@mui/material";
 
 export default function Index() {
-  return (
-            <AuthenticatedLayout
-                header={
-                    <h2 className="text-xl font-semibold leading-tight text-gray-800">
-                        Sample
-                    </h2>
-                }
-            >
-                <Head title="Dashboard" />
-    
-                <div className="py-12">
-                    <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                        <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                            <div className="p-6 text-gray-900">
-                                Sample
-                            </div>
+    return (
+        <AuthenticatedLayout
+            header={
+                <h2 className="text-xl font-semibold leading-tight text-gray-800">
+                    Sample
+                </h2>
+            }
+        >
+            <Head title="Sample" />
+
+            <div className="py-12">
+                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">Sample</div>
+                        <div className="p-6 text-gray-900">
+                            <Container maxWidth="sm">
+                                <Head title="Home" />
+                                <Typography
+                                    variant="h4"
+                                    component="h1"
+                                    gutterBottom
+                                >
+                                    MUI with Inertia + React
+                                </Typography>
+                                <Button variant="contained" color="primary">
+                                    Button
+                                </Button>
+                            </Container>
+                        </div>
+                        <div className="p-6 text-gray-900">
+                            <Box display="flex" flexDirection="row" gap={2}>
+                                <div>flexRowItem1</div>
+                                <div>flexRowItem2</div>
+                                <div>flexRowItem3</div>
+                            </Box>
+                        </div>
+                        <div className="p-6 text-gray-900">
+                            <Box display="flex" flexDirection="column" gap={2}>
+                                <div>flexColumnItem1</div>
+                                <div>flexColumnItem2</div>
+                                <div>flexColumnItem3</div>
+                            </Box>
                         </div>
                     </div>
                 </div>
-            </AuthenticatedLayout>
-  )
+            </div>
+        </AuthenticatedLayout>
+    );
 }
--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,25 +1,44 @@
-import '../css/app.css';
-import './bootstrap';
+import "../css/app.css";
+import "./bootstrap";
 
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createRoot } from 'react-dom/client';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createInertiaApp } from "@inertiajs/react";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { createRoot } from "react-dom/client";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
+
+const theme = createTheme({
+    palette: {
+        mode: "light",
+        primary: {
+            main: "#1976d2",
+        },
+        secondary: {
+            main: "#dc004e",
+        },
+    },
+});
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.tsx`,
-            import.meta.glob('./Pages/**/*.tsx'),
+            import.meta.glob("./Pages/**/*.tsx")
         ),
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <App {...props} />
+            </ThemeProvider>
+        );
     },
     progress: {
-        color: '#4B5563',
+        color: "#4B5563",
     },
 });
